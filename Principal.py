b8c721664b16dbf3427fa8cdbd25260d05dc095d
junior <freesoftandskate@gmail.com>
2018-02-12 13:11:03 -0500

    as

--- Principal.py
+++ Principal.py
@@ -1,10 +1,11 @@
 from PIL import Image, ImageDraw, ImageFont
 
-image = Image.open("certi.jpg")
+image = Image.open("certi3.jpg")
 draw = ImageDraw.Draw(image)
 
 # O bien /usr/share/fonts/truetype/ttf-dejavu/DejaVuSerif.ttf.
-
+#841.89 595.28 a4
+# 65/325
 font = ImageFont.truetype("arial.ttf", 28)
-draw.text((550, 390), "JUNIOR GROVER FLORES MARTINEZ ", font=font, fill="black")
+draw.text((65, 295), "JUNIOR GROVER FLORES MARTINEZ ", font=font, fill="black")
 image.save("salida.jpg")
