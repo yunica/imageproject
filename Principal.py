daeb08d5eb2a30669b2527442e9e87999e83b689
junior <freesoftandskate@gmail.com>
2018-02-13 10:27:20 -0500

    Agregando fuentes

--- Principal.py
+++ Principal.py
@@ -2,10 +2,12 @@ from PIL import Image, ImageDraw, ImageFont
 
 image = Image.open("certi3.jpg")
 draw = ImageDraw.Draw(image)
-
+fuente = open("fonts/AmazDooMLeft.ttf")
 # O bien /usr/share/fonts/truetype/ttf-dejavu/DejaVuSerif.ttf.
-#841.89 595.28 a4
+# 841.89 595.28 a4
 # 65/325
-font = ImageFont.truetype("arial.ttf", 28)
-draw.text((65, 295), "JUNIOR GROVER FLORES MARTINEZ ", font=font, fill="black")
+# font = ImageFont.truetype("arial.ttf", 28)
+font = ImageFont.truetype("fonts/centurygotic.ttf", 28)
+
+draw.text((65, 295), "JUNIOR GROVER FLORES MARTINEZ ", font=font, fill="#e20f0f")
 image.save("salida.jpg")
